769cefc01c09888f214e09b963f530cd45cb06ff
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-22 15:56:33 +0100

    add header and jumbotron including them in main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,7 @@
 </head>
 <body>
     @include('partials.header')
+    @include('partials.jumbotron')
 
     @yield('content')
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,47 @@
 <header>
-    HEADER
-    <a href="{{route('home')}}">Home</a>
-    <a href="{{route('characterCard')}}">Character Card</a>
+    <div class="header-top">
+        <div class="container">
+            <a href="#">dc power visa</a>
+            <a href="#">additional dc sites</a>
+        </div>
+    </div>
+    <div class="container nav-bar">
+        <div class="logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="logo Dc" />
+        </div>
+        <nav>
+            <ul>
+                <li >
+                <a href="#">characters</a>
+                </li>
+                <li >
+                    <a href="#">comics</a>
+                </li>
+                <li >
+                    <a href="#">movies</a>
+                </li>
+                <li >
+                    <a href="#">tv</a>
+                </li>
+                <li >
+                    <a href="#">games</a>
+                </li>
+                <li >
+                    <a href="#">collectibles</a>
+                </li>
+                <li >
+                    <a href="#">videos</a>
+                </li>
+                <li >
+                    <a href="#">fans</a>
+                </li>
+                <li >
+                    <a href="#">news</a>
+                </li>
+                <li >
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
