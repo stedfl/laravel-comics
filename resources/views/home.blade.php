56aac4350d0c3865cbecbfb5fd365c03f1e15a56
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-22 19:17:32 +0100

    complete content in home view and link cards to single card view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,28 @@
         <div class="cards">
             @foreach ($comics as $comic )
             <div class="card">
-                <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                <a href="{{route('character')}}">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                </a>
                 <h3>{{$comic['series']}}</h3>
             </div>
             @endforeach
             <button>load more</button>
         </div>
+    </div>
+    <div class="buy-bar">
+        <nav>
+          <ul>
+            @foreach ($menuBuy as $item )
+                <li>
+                    <div class="buy-card">
+                        <img src="{{Vite::asset('resources/img/'.$item['pathImage'])}}" alt="{{$item['text']}} logo">
+                    </div>
+                    <a href="#">{{$item['text']}}</a>
+                </li>
+            @endforeach
+          </ul>
+        </nav>
     </div>
 </main>
 @endsection
