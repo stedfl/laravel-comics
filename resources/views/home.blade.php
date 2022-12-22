d80108fa00553fe2c5cfe04630193a299e3f081f
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-22 18:13:40 +0100

    add dinamic print of comics card in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-<main>
-    MAIN HOME
+<main class="home">
+    <div class="container">
+        <span class="current">current series</span>
+        <div class="cards">
+            @foreach ($comics as $comic )
+            <div class="card">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                <h3>{{$comic['series']}}</h3>
+            </div>
+            @endforeach
+            <button>load more</button>
+        </div>
+    </div>
 </main>
 @endsection
